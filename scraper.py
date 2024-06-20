157430474a53e917be926db726598f2c3b5b6914
kabinh07 <kanchon@polygontech.xyz>
2024-06-20 15:19:41 +0600

    before checking pop up

--- scraper.py
+++ scraper.py
@@ -10,7 +10,7 @@ with open("categories.txt", "r") as f:
 def get_domain(url):
     elms = url.split("/")
     for elm in elms:
-        if ".com" in elm:
+        if ".com" in elm or ".net" in elm:
             return elm
     return
 
@@ -33,15 +33,20 @@ def news_search(driver, base_url):
     hlinks = driver.find_elements(By.TAG_NAME, "a")
     for link in tqdm(hlinks, total = len(hlinks)):
         url = link.get_attribute("href")
+        # print(f"Raw URL: {url}")
         if domain in str(url):
             valid = news_validation(url)
             if valid:
                 print(url)
 
 def main():
-    keyword = "বিশ্বকাপ"
-    # base_url = f"https://www.prothomalo.com/search?q={keyword}"
-    base_url = f"https://samakal.com/search?q={keyword}"
+    # keyword = "বিশ্বকাপ"
+    keyword = input("Enter your keyword: ")
+    base_url = f"https://www.prothomalo.com/search?q={keyword}"
+    # base_url = f"https://samakal.com/search?q={keyword}"
+    # base_url = f"https://www.ucanews.com/home/googlesearch?q={keyword}"
+    # base_url = f"https://www.thestatesman.com/?s={keyword}"
+    # base_url = f"https://www.thedailystar.net/search?t={keyword}"
 
     print(categories)
 
