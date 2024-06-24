2473eafcde28c53de2224b125a6850a90a43808c
kabinh07 <kanchon@polygontech.xyz>
2024-06-24 16:14:44 +0600

    using categoreis

--- scraper.py
+++ scraper.py
@@ -33,7 +33,7 @@ def news_search(driver, base_url):
     hlinks = driver.find_elements(By.TAG_NAME, "a")
     for link in tqdm(hlinks, total = len(hlinks)):
         url = link.get_attribute("href")
-        # print(f"Raw URL: {url}")
+        print(f"Raw URL: {url}")
         if domain in str(url):
             valid = news_validation(url)
             if valid:
@@ -41,15 +41,17 @@ def news_search(driver, base_url):
 
 def main():
     # keyword = "বিশ্বকাপ"
-    keyword = input("Enter your keyword: ")
-    base_url = f"https://www.prothomalo.com/search?q={keyword}"
-    # base_url = f"https://samakal.com/search?q={keyword}"
-    # base_url = f"https://www.ucanews.com/home/googlesearch?q={keyword}"
+    keyword = "world cup"
+    # keyword = input("Enter your keyword: ")
+
+    # # URLs
     # base_url = f"https://www.thestatesman.com/?s={keyword}"
-    # base_url = f"https://www.thedailystar.net/search?t={keyword}"
-
-    print(categories)
-
+    # base_url = f"https://www.thenews.com.pk/search1?cx=014389848304649563256%253Aureyfztd74a&cof=FORID%253A10&ie=UTF-8&q={keyword}"
+    # base_url = f"https://www.telegraphindia.com/search?search-term={keyword}"
+    # base_url = f"https://www.taiwannews.com.tw/search?keyword{keyword}"
+    # base_url = f"https://www.sentinelassam.com/search?q={keyword}"
+    base_url = f"https://www.ndtv.com/search?searchtext={keyword}"
+    
     driver = webdriver.Chrome()
     news_search(driver, base_url)
     driver.quit()
